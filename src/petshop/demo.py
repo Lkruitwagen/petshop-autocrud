1322e4c1a59f1d6d5d3bbfc300c80024125e1b43
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-12-21 04:13:49 +0000

    feat: add mixins to demo

--- src/petshop/demo.py
+++ src/petshop/demo.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel, Field, create_engine
 from pydantic import BaseModel
+from datetime import date, datetime
 
-from petshop.mixins import ReadMixin, ReadParams, RouterParams, CreateParams, CreateMixin
+from petshop.mixins import (
+    ReadMixin, 
+    ReadParams, 
+    RouterParams, 
+    CreateParams, 
+    CreateMixin, 
+    SearchMixin,
+    SearchParams,
+    DeleteMixin,
+    DeleteParams,
+    PatchMixin,
+    PatchParams,
+)
 from petshop.core.database import engine
 from petshop.core.config import settings
 
@@ -10,7 +23,7 @@ from petshop.core.config import settings
 ReadMixin.__config__ = None
 
 
-class Human(SQLModel, ReadMixin, CreateMixin, table=True):
+class Human(SQLModel, ReadMixin, CreateMixin, SearchMixin,  DeleteMixin, PatchMixin, table=True):
     id: int | None = Field(default=None, primary_key=True)
     first_name: str | None
     last_name: str | None
@@ -28,6 +41,29 @@ class Human(SQLModel, ReadMixin, CreateMixin, table=True):
         summary = "Create human"
         operation_id = "create_human"
 
+    class search_cfg(SearchParams):
+        required_params = []
+        pop_params=["id"]
+        description = "Search human"
+        summary = "Search human"
+        operation_id = "search_human"
+
+        # for str:
+        search_contains = True # string contains search
+        search_trgm = ["last_name"] # string trigram search
+
+    class delete_cfg(DeleteParams):
+        primary_key = "id"
+        description = "Delete human"
+        summary = "Delete human"
+        operation_id = "delete_human"
+
+    class patch_cfg(PatchParams):
+        primary_key = "id"
+        description = "Patch human"
+        summary = "Patch human"
+        operation_id = "patch_human"
+
     class router_cfg(RouterParams):
         prefix = "/humans"
         tags=["humans"]
@@ -36,9 +72,10 @@ class Human(SQLModel, ReadMixin, CreateMixin, table=True):
 
 
 
-class Pet(SQLModel, ReadMixin, CreateMixin, table=True):
+class Pet(SQLModel, ReadMixin, CreateMixin, SearchMixin, DeleteMixin, PatchMixin, table=True):
     id: int | None = Field(default=None, primary_key=True)
     species: str
+    birthday: date
 
     class read_cfg(ReadParams):
         primary_key = 'id'
@@ -53,6 +90,29 @@ class Pet(SQLModel, ReadMixin, CreateMixin, table=True):
         summary = "Create pet"
         operation_id = "create_pet"
 
+    class search_cfg(SearchParams):
+        required_params = []
+        pop_params=["id"]
+        description = "Search pet"
+        summary = "Search pet"
+        operation_id = "search_pet"
+
+        search_eq = True # is precisely equal to
+        search_lt = True # less than, list[str] | bool
+        search_lte = True # less than or equal to, list[str] | bool
+
+    class delete_cfg(DeleteParams):
+        primary_key = "id"
+        description = "Delete pet"
+        summary = "Delete pet"
+        operation_id = "delete_pet"
+
+    class patch_cfg(PatchParams):
+        primary_key = "id"
+        description = "Patch pet"
+        summary = "Patch pet"
+        operation_id = "patch_pet"
+
     class router_cfg(RouterParams):
         prefix = "/pets"
         tags=["pets"]
@@ -60,9 +120,17 @@ class Pet(SQLModel, ReadMixin, CreateMixin, table=True):
 
 
 Human.build_read_route()
-Pet.build_read_route()
 Human.build_create_route()
+Human.build_search_route()
+Human.build_delete_route()
+Human.build_patch_route()
+
+
+Pet.build_read_route()
 Pet.build_create_route()
+Pet.build_search_route()
+Pet.build_delete_route()
+Pet.build_patch_route()
 
 # create tables
 #SQLModel.metadata.create_all(engine)
